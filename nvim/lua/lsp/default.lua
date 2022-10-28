d5e9ec027a03b33e328a9c664839446127155242
tonic <tonicbupt@gmail.com>
2022-10-28 14:54:49 +0800

    update lsp

--- nvim/lua/lsp/default.lua
+++ nvim/lua/lsp/default.lua
@@ -23,7 +23,7 @@ local function on_attach(_, bufnr)
   map("n", "gl", vim.diagnostic.open_float)
 end
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(
+local capabilities = require("cmp_nvim_lsp").default_capabilities(
   vim.lsp.protocol.make_client_capabilities()
 )
 
