965a74915177d67c97f754c858e71fa6ac7d774e
tonic <li.yang@firstwavetech.net>
2024-05-13 15:33:36 +0800

    some changes

--- nvim/lua/lsp/servers/tsserver.lua
+++ nvim/lua/lsp/servers/tsserver.lua
@@ -1,14 +1,21 @@
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
   callback = function()
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        only = { "source.organizeImports.ts" },
+        diagnostics = {},
+      },
+    })
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        only = { "source.removeUnused.ts" },
+        diagnostics = {},
+      },
+    })
     vim.lsp.buf.format()
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
-  callback = function()
-    vim.lsp.buf.execute_command({ command = "_typescript.organizeImports", arguments = { vim.api.nvim_buf_get_name(0) } })
   end,
 })
 
