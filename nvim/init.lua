fbd8eb8a781698ecabf775a98a12678844c50a5c
tonic <tonicbupt@gmail.com>
2024-06-27 21:38:04 +0800

    show relative path

--- nvim/init.lua
+++ nvim/init.lua
@@ -434,7 +434,7 @@ require("lazy").setup({
         sections = {
           lualine_a = { "mode" },
           lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = { { "filename", path = 2 } },
+          lualine_c = { { "filename", path = 1 } },
           lualine_x = { "encoding", "fileformat", "filetype" },
           lualine_y = { "progress" },
           lualine_z = { "location" },
