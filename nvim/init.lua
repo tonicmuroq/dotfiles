6718415d837a7b871cf5f68d44c9991f9e7eb5bd
tonic <tonicbupt@gmail.com>
2024-06-27 21:32:03 +0800

    show absolute path

--- nvim/init.lua
+++ nvim/init.lua
@@ -427,14 +427,14 @@ require("lazy").setup({
     config = function()
       require("lualine").setup({
         options = {
-          theme = "onedark",
+          theme = "oceanicnext",
           component_separators = { left = "|", right = "|" },
           section_separators = { left = "", right = "" },
         },
         sections = {
           lualine_a = { "mode" },
           lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = { "filename" },
+          lualine_c = { { "filename", path = 2 } },
           lualine_x = { "encoding", "fileformat", "filetype" },
           lualine_y = { "progress" },
           lualine_z = { "location" },
