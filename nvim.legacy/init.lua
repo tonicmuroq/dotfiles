6ed9545d958aeabc0e32dc0c852ff5bead8fdff8
tonic <li.yang@firstwavetech.net>
2024-06-27 16:49:36 +0800

    brand new nvim config!

---
+++ nvim.legacy/init.lua
@@ -0,0 +1,12 @@
+require("options")
+require("keymaps")
+require("plugins")
+require("lsp")
+require("autocmd")
+
+
+-- require('filetype').setup({
+--   extensions = {
+--     move = 'move',
+--   }
+-- })
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,487 @@
-require("options")
-require("keymaps")
-require("plugins")
-require("lsp")
-require("autocmd")
+-- options
+vim.opt.fileencoding = "UTF-8"
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.updatetime = 300
+vim.opt.background = "dark"
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.smartcase = true
+vim.opt.mouse = ""
+vim.opt.signcolumn = "yes"
+vim.opt.pumheight = 15
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.termguicolors = true
+vim.opt.timeoutlen = 300
+vim.opt.list = true
+vim.opt.listchars = {
+  nbsp = "+",
+  tab = "  ",
+  trail = "-",
+}
 
+-- keymaps
+vim.keymap.set("i", "jk", "<Esc>", { desc = "jk to <Esc>" })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set(
+  "n",
+  "<Esc>",
+  "<cmd>nohlsearch<CR>",
+  { desc = "Clear search highlights when press <Esc> in normal mode" }
+)
 
--- require('filetype').setup({
---   extensions = {
---     move = 'move',
---   }
--- })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+
+-- autocmds
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  desc = "Highlight on yank",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go,make,gitconfig",
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.expandtab = false
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "html,css,javascript,typescript,typescriptreact,scss,json,yaml,lua",
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  command = [[ set fo-=c fo-=r fo-=o ]],
+  desc = "do not auto commenting new lines",
+})
+
+-- plugins
+
+-- init lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- setup lazy.nvim and all the plugins
+require("lazy").setup({
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "nvim-telescope/telescope-ui-select.nvim",
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable("make")
+        end,
+      },
+    },
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local builtin = require("telescope.builtin")
+      local themes = require("telescope.themes")
+
+      telescope.setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-u>"] = false,
+              ["<C-d>"] = false,
+              ["<C-[>"] = actions.close,
+            },
+          },
+          dynamic_preview_title = true,
+        },
+        extensions = {
+          ["fzf"] = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
+          ["ui-select"] = {
+            themes.get_dropdown(),
+          },
+        },
+      })
+      telescope.load_extension("fzf")
+      telescope.load_extension("live_grep_args")
+      telescope.load_extension("ui-select")
+
+      vim.keymap.set("n", "<leader>f", builtin.find_files)
+      vim.keymap.set("n", "<leader>o", builtin.oldfiles)
+      vim.keymap.set("n", "<leader>bb", builtin.buffers)
+      vim.keymap.set("n", "<leader>rg", builtin.grep_string)
+      vim.keymap.set("n", "<leader>xx", builtin.diagnostics)
+      vim.keymap.set("n", "<leader>co", builtin.lsp_document_symbols)
+      vim.keymap.set("n", "<leader>xb", function()
+        builtin.diagnostics({ bufnr = 0 })
+      end)
+      vim.keymap.set("n", "gd", builtin.lsp_definitions)
+      vim.keymap.set("n", "gt", builtin.lsp_type_definitions)
+      vim.keymap.set("n", "gr", builtin.lsp_references)
+      vim.keymap.set("n", "gi", builtin.lsp_implementations)
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup({
+        triggers = { "<leader>" },
+      })
+    end,
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+      require("nvim-tree").setup({
+        view = {
+          adaptive_size = true,
+        },
+        update_focused_file = {
+          enable = true,
+        },
+      })
+    end,
+    keys = {
+      { "<leader>ee", "<cmd>NvimTreeToggle<cr>", desc = "NvimTree" },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        modules = {},
+        ensure_installed = {},
+        auto_install = true,
+        sync_install = false,
+        ignore_install = {},
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        config = true,
+      },
+      { "williamboman/mason-lspconfig.nvim" },
+      {
+        "j-hui/fidget.nvim",
+        opts = {
+          notification = {
+            window = {
+              winblend = 0,
+            },
+          },
+        },
+      },
+      {
+        "folke/neodev.nvim",
+        opts = {},
+      },
+    },
+    config = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+        callback = function(event)
+          local map = function(keys, func, desc)
+            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+          end
+
+          map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+          map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+          map("K", vim.lsp.buf.hover, "Hover Documentation")
+
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+            map("<leader>th", function()
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 }))
+            end, "[T]oggle Inlay [H]ints")
+          end
+        end,
+      })
+
+      local servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              completion = {
+                callSnippet = "Replace",
+              },
+            },
+          },
+        },
+        bashls = {},
+        clangd = {
+          cmd = { "clangd", "--function-arg-placeholders=0" },
+        },
+        cssls = {},
+        gopls = {},
+        html = {},
+        jsonls = {},
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                typeCheckingMode = "off",
+                diagnosticSeverityOverrides = {
+                  reportUnusedFunction = "off",
+                },
+              },
+            },
+          },
+        },
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              imports = {
+                granularity = {
+                  group = "crate",
+                  enforce = true,
+                },
+              },
+              checkOnSave = {
+                command = "clippy",
+              },
+              completion = {
+                callable = {
+                  snippets = "add_parentheses",
+                },
+                postfix = {
+                  enable = false,
+                },
+              },
+              workspace = {
+                symbol = {
+                  search = {
+                    kind = "all_symbols",
+                  },
+                },
+              },
+            },
+          },
+        },
+        taplo = {},
+        tsserver = {},
+        yamlls = {
+          settings = {
+            yaml = {
+              keyOrdering = false,
+            },
+          },
+        },
+        volar = {},
+      }
+
+      local ensure_installed = vim.tbl_keys(servers or {})
+
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+      local lspconfig = require("lspconfig")
+
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = ensure_installed,
+        automatic_installation = true,
+        handlers = {
+          function(server_name)
+            local server = servers[server_name] or {}
+            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+            lspconfig[server_name].setup(server)
+          end,
+        },
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    lazy = false,
+    keys = {
+      {
+        "<leader>w",
+        function()
+          require("conform").format({ async = true, lsp_fallback = true })
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        local disable_filetypes = { c = true, cpp = true }
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
+      formatters_by_ft = {
+        go = { "goimports", "gofumpt" },
+        javascript = { { "prettierd", "prettier" } },
+        typescript = { "prettier" },
+      },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-vsnip",
+      "hrsh7th/vim-vsnip",
+      "rafamadriz/friendly-snippets",
+      "onsails/lspkind.nvim",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local lspkind = require("lspkind")
+
+      cmp.setup({
+        completion = { completeopt = "menu,menuone,noinsert" },
+
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "vsnip" },
+          { name = "path" },
+          { name = "buffer",  keyword_length = 4 },
+        },
+        formatting = {
+          fields = { "kind", "abbr", "menu" },
+          expandable_indicator = true,
+          format = lspkind.cmp_format({
+            mode = "symbol_text",
+          }),
+        },
+      })
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      { "nvim-tree/nvim-web-devicons" },
+    },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "onedark",
+          component_separators = { left = "|", right = "|" },
+          section_separators = { left = "", right = "" },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { "filename" },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+      })
+    end,
+  },
+  { "stevearc/dressing.nvim" },
+  { "lifepillar/pgsql.vim" },
+  { "uarun/vim-protobuf" },
+  { "maralla/gomod.vim" },
+  { "github/copilot.vim" },
+  {
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    init = function()
+      vim.cmd("colorscheme kanagawa")
+    end,
+    config = function()
+      require("kanagawa").setup({
+        compile = false,
+        undercurl = true,
+        commentStyle = { italic = false },
+        functionStyle = {},
+        keywordStyle = { italic = false },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false,
+        dimInactive = false,
+        terminalColors = true,
+        colors = {
+          palette = {
+            peachRed = "#E46876",
+          },
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors)
+          return {
+            ["@string.special.url"] = { fg = colors.theme.syn.special1, undercurl = false },
+            ["@variable.builtin"] = { fg = colors.theme.syn.special2, italic = false },
+          }
+        end,
+        background = {
+          dark = "wave",
+          light = "lotus",
+        },
+      })
+    end,
+  },
+})
