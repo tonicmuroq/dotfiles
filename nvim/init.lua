683e519d4b70df404a3311395d0fc7b90bbede79
tonic <li.yang@firstwavetech.net>
2024-07-01 14:37:29 +0800

    still prefer onedark

--- nvim/init.lua
+++ nvim/init.lua
@@ -427,7 +427,7 @@ require("lazy").setup({
     config = function()
       require("lualine").setup({
         options = {
-          theme = "oceanicnext",
+          theme = "onedark",
           component_separators = { left = "|", right = "|" },
           section_separators = { left = "", right = "" },
         },
