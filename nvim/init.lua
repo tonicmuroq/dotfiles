42425056dc456d1dcaa4cf5c1c43a6760d2dcab1
tonic <li.yang@firstwavetech.net>
2024-07-02 10:48:24 +0800

    disable formatter on protobuf

--- nvim/init.lua
+++ nvim/init.lua
@@ -363,7 +363,9 @@ require("lazy").setup({
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = {
+          proto = true,
+        }
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
