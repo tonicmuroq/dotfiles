6390ee0588ffbf3ac708809c729e90426be12a8f
tonic <li.yang@firstwavetech.net>
2024-09-09 14:43:12 +0800

    copied from lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -576,16 +576,23 @@ require("lazy").setup({
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
+    build = ":Copilot auth",
     event = "InsertEnter",
-    config = function()
-      require("copilot").setup()
-    end,
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
   },
   {
     "zbirenbaum/copilot-cmp",
+    dependencies = "copilot.lua",
     config = function()
       require("copilot_cmp").setup()
-    end
+    end,
   },
   {
     "rebelot/kanagawa.nvim",
