65e90946b649b54de00b06803de77ec5a2b9a572
tonic <li.yang@firstwavetech.net>
2024-10-21 16:52:56 +0800

    try meslo font

--- nvim/init.lua
+++ nvim/init.lua
@@ -531,8 +531,6 @@ require("lazy").setup({
       require("lualine").setup({
         options = {
           theme = "onedark",
-          component_separators = { left = "|", right = "|" },
-          section_separators = { left = "", right = "" },
         },
         sections = {
           lualine_a = { "mode" },
