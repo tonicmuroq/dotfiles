9a550dbe375dd78844daed74f7d8fc5e788cd3dc
tonic <li.yang@firstwavetech.net>
2024-11-04 10:26:13 +0800

    add bufferline

--- nvim/init.lua
+++ nvim/init.lua
@@ -580,6 +580,32 @@ require("lazy").setup({
     end,
   },
   {
+    "akinsho/bufferline.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>",            desc = "Toggle Pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+      { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>",          desc = "Delete Other Buffers" },
+      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>",           desc = "Delete Buffers to the Right" },
+      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>",            desc = "Delete Buffers to the Left" },
+      { "<S-h>",      "<cmd>BufferLineCyclePrev<cr>",            desc = "Prev Buffer" },
+      { "<S-l>",      "<cmd>BufferLineCycleNext<cr>",            desc = "Next Buffer" },
+      { "[b",         "<cmd>BufferLineCyclePrev<cr>",            desc = "Prev Buffer" },
+      { "]b",         "<cmd>BufferLineCycleNext<cr>",            desc = "Next Buffer" },
+      { "[B",         "<cmd>BufferLineMovePrev<cr>",             desc = "Move buffer prev" },
+      { "]B",         "<cmd>BufferLineMoveNext<cr>",             desc = "Move buffer next" },
+    },
+    opts = {
+      options = {
+        always_show_bufferline = false,
+        auto_toggle_bufferline = true,
+      },
+    },
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+    end,
+  },
+  {
     "nvimdev/dashboard-nvim",
     event = "VimEnter",
     config = function()
